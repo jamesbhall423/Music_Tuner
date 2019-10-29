5395cd8b2a1d53e9adf9214b8306c16a3cb9ecd3
joshvspendlove <Joshvspendlove@gmail.com>
2019-10-28 19:51:45 -0600

    Josh Spendlove Commit

--- app/src/main/java/school/team/musictuner/MainActivity.java
+++ app/src/main/java/school/team/musictuner/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 //James Hall comment1
+//Josh Spendlove Comment 2
 public class MainActivity extends AppCompatActivity {
 
     @Override
