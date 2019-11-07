086be99afaa0b39ac3a54829a698b9cc64edeaec
James Hall <jamesbhall423@byui.edu>
2019-11-06 20:09:34 -0800

    Create base Converter and Settings class

---
+++ app/src/main/java/school/team/musictuner/Converter.java
@@ -0,0 +1,11 @@
+package school.team.musictuner;
+
+public class Converter {
+    private Settings settings;
+    public void setSettings(Settings settings) {
+        this.settings=settings;
+    }
+    public Signal getSignal(Sound sound, int on, int off) {
+        return null;
+    }
+}
---
+++ app/src/main/java/school/team/musictuner/Settings.java
@@ -0,0 +1,7 @@
+package school.team.musictuner;
+
+public class Settings {
+    public void setSensitivity(double sensitivity) {
+
+    }
+}
