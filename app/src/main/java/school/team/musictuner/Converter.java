1680713cbd320cdd0332e392d4755bbf3f52e161
James Hall <jamesbhall423@byui.edu>
2019-11-06 20:24:16 -0800

    Update Converter and Settings class

--- app/src/main/java/school/team/musictuner/Converter.java
+++ app/src/main/java/school/team/musictuner/Converter.java
@@ -5,6 +5,9 @@ public class Converter {
     public void setSettings(Settings settings) {
         this.settings=settings;
     }
+    public Settings getSettings() {
+        return settings;
+    }
     public Signal getSignal(Sound sound, int on, int off) {
         return null;
     }
--- app/src/main/java/school/team/musictuner/Settings.java
+++ app/src/main/java/school/team/musictuner/Settings.java
@@ -1,7 +1,11 @@
 package school.team.musictuner;
 
 public class Settings {
+    private double sensitivity;
     public void setSensitivity(double sensitivity) {
-
+        this.sensitivity=sensitivity;
+    }
+    public double getSensitivity() {
+        return sensitivity;
     }
 }
