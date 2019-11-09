d48dc39feddf271ec83c7690148e0ce5691a000a
James Hall <jamesbhall423@byui.edu>
2019-11-09 14:02:06 -0800

    Converter documentation

--- app/src/main/java/school/team/musictuner/Converter.java
+++ app/src/main/java/school/team/musictuner/Converter.java
@@ -1,5 +1,8 @@
 package school.team.musictuner;
 
+/**
+ * Provides a list of useful methods converting between different types of sound classes.
+ */
 public class Converter {
     private Settings settings;
     private Tuner tuner;
@@ -9,16 +12,41 @@ public class Converter {
     public Settings getSettings() {
         return settings;
     }
+
+    /**
+     * Constructs a Signal from the given sound
+     * @param sound
+     * @param on the beginning sample, inclusive
+     * @param off the end sample, not inclusive
+     * @return
+     */
     public Signal getSignal(Sound sound, int on, int off) {
         return null;
     }
-    Signal getSignal(Sound sound) {
+
+    /**
+     * Constructs a Signal from the given Sound
+     * @param sound
+     * @return
+     */
+    public Signal getSignal(Sound sound) {
         return new Signal();
     }
 
+    /**
+     * Constructs a Timeline from the given sound
+     * @param sound
+     * @return
+     */
     public Timeline soundTimeline(Sound sound) {
         return null;
     }
+
+    /**
+     * Constructs a PlayedSection from the given Timeline
+     * @param timeline
+     * @return
+     */
     public PlayedSection section(Timeline timeline) {
         return null;
     }
