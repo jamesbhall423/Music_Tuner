c9dd3ec571b6ffe9625bf3c4f19c73165f487f9f
James Hall <jamesbhall423@byui.edu>
2019-11-09 12:47:10 -0800

    Display interface documentation

--- app/src/main/java/school/team/musictuner/MainDisplay.java
+++ app/src/main/java/school/team/musictuner/MainDisplay.java
@@ -1,7 +1,14 @@
 package school.team.musictuner;
-
+/*
+* Represents the entrance to the app. The main activity will implement this interface.
+* Works in conjunction with MainController
+ */
 public interface MainDisplay {
-    void displayNote(Pitch note);
+    /*
+    * Displays the given pitch and its deviation from the ideal note.
+    * Must be called on UI thread
+     */
+    void displayNote(Pitch pitch, Note note);
 
     void runOnUIThread(Runnable runnable);
 
--- app/src/main/java/school/team/musictuner/TrainingDisplay.java
+++ app/src/main/java/school/team/musictuner/TrainingDisplay.java
@@ -1,5 +1,9 @@
 package school.team.musictuner;
-
+/*
+* An interface that represents an activity to train the Tuner to the precise overtones of a given instrument.
+* Works with Tuner object
+* Stretch
+ */
 public interface TrainingDisplay {
     
 }
