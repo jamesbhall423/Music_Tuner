a1e5ae48e9eb0f8ccf03ef0dc5bf9b4ad2fb05c5
Zaquen <zaqnelson@yahoo.com>
2019-11-11 16:19:16 -0700

    Just making Sure

--- app/src/main/java/school/team/musictuner/AdvancedActivity.java
+++ app/src/main/java/school/team/musictuner/AdvancedActivity.java
@@ -1,4 +1,15 @@
 package school.team.musictuner;
 
-public class AdvancedActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdvancedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanced);
+    }
 }
+
--- app/src/main/java/school/team/musictuner/SettingsActivity.java
+++ app/src/main/java/school/team/musictuner/SettingsActivity.java
@@ -1,4 +1,14 @@
 package school.team.musictuner;
 
-public class SettingsActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
 }
