df2f1f2b6c1ab7c3f3c5f8dd8ece2f04ffa256e5
Grunter15 <moo15031@byui.edu>
2019-11-25 19:49:41 -0700

    Adding Javadoc to the file.

--- app/src/main/java/school/team/musictuner/TrainingActivity.java
+++ app/src/main/java/school/team/musictuner/TrainingActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Brings up the activity to help the user tune their instruments
+ */
 public class TrainingActivity extends AppCompatActivity {
 
     public static final String TRAINING_ACTIVITY_TAG = "Tuner TrainingActivity";
@@ -16,14 +19,22 @@ public class TrainingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
     }
+
+    /*
+    * This will change the view to what the user changed the settings to.
+    */
     public void loadAdvanced(View view) {
         Intent intentLoad = new Intent(this, AdvancedActivity.class);
         startActivity(intentLoad);
     }
+
+    //Checking any changes of the settings by the user
     public void loadSettings(View view) {
         Intent intentLoad = new Intent(this, SettingsActivity.class);
         startActivity(intentLoad);
     }
+
+    //Loading the main view
     public void loadMain(View view) {
         Intent intentLoad = new Intent(this, MainActivity.class);
         startActivity(intentLoad);
